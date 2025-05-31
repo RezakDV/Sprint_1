9f3b5a996b7c2ac1f9e5d82a7d037938e804353b
Denis Reznichenko <dwreznichenko@gmail.com>
2025-05-31 22:57:34 +0200

    Fix method pop() in task_4.py

--- task_4.py
+++ task_4.py
@@ -2,7 +2,7 @@ new_tasks = ['task_001', 'task_011', 'task_007', 'task_015', 'task_005']
 completed_tasks = ['task_002', 'task_012', 'task_006']
 
 # Переносим элемент task_005 из списка new_tasks в completed_tasks
-completed_tasks.append(new_tasks.pop(4))
+completed_tasks.append(new_tasks.pop())
 
 # Удаляем элемент task_007 из списка new_tasks
 new_tasks.remove('task_007')
